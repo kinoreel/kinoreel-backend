e7c5c45ff7f4015c5e53790bdc9fda2671ae956d
Ted Johansson <tedjohanssondeveloper@gmail.com>
2017-04-27 17:53:08 +0100

    Added the first view that send a hello world message over a rest api. Created the rest api framework for automated generation of api urls.

---
+++ movies/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+
+from rest_framework_nested import routers
+
+from movies import views
+
+
+movie_router = routers.SimpleRouter()
+movie_router.register(r'movies', views.MovieViewSet, base_name='movies')
+
+urlpatterns = [
+    url(r'^', include(movie_router.urls))
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+
+class MovieViewSet(mixins.RetrieveModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    """
+    Main class for all the api pages, self generating url if list_route is used
+    """
+    @list_route(permission_classes=[],
+                methods=['GET'])
+    def hello_world(self, request):
+        """
+        Just a test
+        :param request: Request data from the api call
+        :return: HTML Response
+        """
+        return Response({'messages': ['Hello World']}, status=200)
