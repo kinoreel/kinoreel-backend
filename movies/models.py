8a5aab6a4ce71624d76184746e1954b6c16859f0
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-07-06 18:41:15 +0100

    Created kino tables

--- movies/models.py
+++ movies/models.py
@@ -5,10 +5,149 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    imdb_id = models.CharField(max_length=10, unique=True)
+    imdb_id = models.CharField(max_length=10, primary_key=True, null=False)
     title = models.CharField(max_length=1000, null=False)
     runtime = models.CharField(max_length=100, null=False)
     rated = models.CharField(max_length=15, null=False)
     released = models.CharField(max_length=15, null=False)
     orig_language = models.CharField(max_length=1000, null=False)
     tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Companies(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1000, null=False, unique=True)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Persons(models.Model):
+    person_id = models.AutoField(primary_key=True)
+    fullname = models.CharField(max_length=1000, null=False, unique=True)
+    sex = models.CharField(max_length=1000, choices=[('F', 'Female'), ('M', 'Male')])
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Festivals(models.Model):
+    imdb_event_id = models.CharField(max_length=10, null=False)
+    name = models.CharField(max_length=1000, null=False)
+    location = models.CharField(max_length=1000, null=False)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Person_Roles(models.Model):
+    role = models.CharField(max_length=1000, primary_key=True)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Company_Roles(models.Model):
+    role = models.CharField(max_length=1000, primary_key=True)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Movie2Awards(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    imdb_event_id = models.ForeignKey(Festivals)
+    award = models.CharField(max_length=10, null=False)
+    position = models.CharField(max_length=1, null=False, choices=[('Y', 'yes'), ('N', 'No')])
+    year = models.CharField(max_length=4, null=False)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('imdb_id', 'year', 'award')
+
+
+class Movies2Companies(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    company_id = models.ForeignKey(Companies)
+    role = models.CharField(max_length=100, null=False)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('imdb_id', 'company_id', 'role')
+
+
+class Movies2Genres(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    genre = models.CharField(max_length=250, primary_key=True)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('imdb_id', 'genre')
+
+
+class Movies2Keywords(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    keyword = models.CharField(max_length=250)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('imdb_id', 'keyword')
+
+
+class Movies2Numbers(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    type = models.CharField(max_length=250)
+    value = models.BigIntegerField(null=False)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Movies2Persons(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    person_id = models.ForeignKey(Persons)
+    role = models.ForeignKey(Person_Roles)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+         unique_together = ('imdb_id', 'person_id', 'role')
+
+
+class Movies2Posters(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    url = models.CharField(max_length=1000)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+         unique_together = ('imdb_id', 'url')
+
+
+class Movies2Ratings(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    source = models.CharField(max_length=250)
+    rating = models.CharField(max_length=100, null=False)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+         unique_together = ('imdb_id', 'source')
+
+
+class Movies2Stats(models.Model):
+    imdb_id = models.OneToOneField(Movie)
+    tmdb_vote_average = models.BigIntegerField()
+    tmdb_vote_count = models.BigIntegerField()
+    imdb_votes = models.BigIntegerField()
+    youtube_likes = models.BigIntegerField()
+    youtube_dislikes= models.BigIntegerField()
+    tstamp = models.DateTimeField(default=timezone.now)
+
+
+class Movies2Streams(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    source = models.CharField(max_length=250, null=False)
+    url = models.CharField(max_length=1000, null=False)
+    currency = models.CharField(max_length=1, null=False, choices=[('£', 'Pound'), ('$', 'Dollar')])
+    price = models.CharField(max_length=250)
+    format = models.CharField(max_length=30)
+    purchase_type = models.CharField(max_length=30)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together=('imdb_id', 'source', 'url', 'format', 'purchase_type')
+
+
+class Movies2Trailers(models.Model):
+    imdb_id = models.ForeignKey(Movie)
+    url = models.CharField(max_length=1000)
+    tstamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+         unique_together = ('imdb_id', 'url')
--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import include, url
-
 from rest_framework_nested import routers
 
 from movies import views
