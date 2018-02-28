2b6fcb362a6dbf875075af13787bba76928098c3
Ted Johansson <tedjohanssondeveloper@gmail.com>
2018-02-28 17:34:46 +0000

    Include is still needed for the url patterns

--- kinoreel_backend/urls.py
+++ kinoreel_backend/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
 
 from movies.urls import urlpatterns as movie_urls
 
 urlpatterns = [
-    url(r'^', movie_urls, name='movies'),
+    url(r'^', include(movie_urls), name='movies'),
     url(r'^admin/', admin.site.urls),
 ]
