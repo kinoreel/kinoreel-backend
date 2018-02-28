01ec1131e014cad992bec915a98bf8bbfe190c11
Ted Johansson <tedjohanssondeveloper@gmail.com>
2018-02-28 17:09:14 +0000

    Made it django 1.9 compatible

--- kinoreel_backend/urls.py
+++ kinoreel_backend/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import login
 from movies.urls import urlpatterns as movie_urls
 
 urlpatterns = [
-    url(r'^', include(movie_urls), name='movies'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', movie_urls, name='movies'),
+    url(r'^admin/', admin.site.urls),
 ]
